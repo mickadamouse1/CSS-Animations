824481c179a71fd69751b75f1689cb9705da9bf9
mickadamouse1 <michaelchambers22@hotmail.co.uk>
2019-09-26 19:48:32 +0100

    Completed grow animation

--- Grow Animation/script.js
+++ Grow Animation/script.js
@@ -1,24 +1,33 @@
 console.log("brans");
 
-var btnGo = document.getElementById("go");
+var btnGo = document.getElementById("linkGo");
 var txtBox = document.getElementById("txtBox");
 var txtName = document.getElementById("txtName");
 var box = document.getElementById("box");
+var appImage = document.getElementById("bkgdBox");
+var txtInfo = document.getElementById("txtInfo");
 
 btnGo.onclick = function() {
-  if (box.style.width === "40rem") {
-    Object.assign(box.style, {
-      width: "18rem",
-      height: "18rem"
-    });
-    txtName.style.display = "block";
-    txtBox.style.justifyContent = "center";
-    txtBox.style.alignItems = "center";
+    toggleAppClass(box);
+    toggleAppClass(txtName);
+    toggleAppClass(txtBox);
+    toggleAppClass(btnGo);
+    toggleAppClass(appImage);
+    toggleAppClass(txtInfo);
+}
+
+function toggleAppClass(element) {
+  var x = element.classList[0];
+  x = x.split("-");
+  x = x[1];
+  i = element.id;
+  if (x === "small") {
+    btnGo.innerHTML = "x";
+    element.classList.toggle(`${i}-small`);
+    element.classList.toggle(`${i}-large`);
   } else {
-    box.style.width = "40rem";
-    box.style.height = "28rem";
-    txtBox.style.justifyContent = "flex-end";
-    txtBox.style.alignItems = "flex-end";
-    txtName.style.opacity = 0;
+    btnGo.innerHTML = "Go";
+    element.classList.toggle(`${i}-large`);
+    element.classList.toggle(`${i}-small`);
   }
 }
